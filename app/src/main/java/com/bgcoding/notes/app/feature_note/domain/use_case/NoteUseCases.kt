6ddfc37e22ff039e19a081bc52bbb1d21dfc7118
Brian Gabini <91449132+briangabini@users.noreply.github.com>
2024-08-23 15:56:16 +0800

    feat: create a wrapper that will contain all use cases for note feature

---
+++ app/src/main/java/com/bgcoding/notes/app/feature_note/domain/use_case/NoteUseCases.kt
@@ -0,0 +1,8 @@
+package com.bgcoding.notes.app.feature_note.domain.use_case
+
+// this class will be a wrapper that will hold all use cases for a single feature
+data class NoteUseCases(
+    val getNotes: GetNotes,
+    val deleteNote: DeleteNote
+) {
+}
