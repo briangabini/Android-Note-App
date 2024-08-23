714d82e085a2adce12577ec05fd2d1ccfd68b49e
Brian Gabini <91449132+briangabini@users.noreply.github.com>
2024-08-23 10:03:48 +0800

    feat: create a Note.kt for the model

---
+++ app/src/main/java/com/bgcoding/notes/app/feature_note/domain/model/Note.kt
@@ -0,0 +1,22 @@
+package com.bgcoding.notes.app.feature_note.domain.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.bgcoding.notes.app.ui.theme.BabyBlue
+import com.bgcoding.notes.app.ui.theme.LightGreen
+import com.bgcoding.notes.app.ui.theme.RedOrange
+import com.bgcoding.notes.app.ui.theme.RedPink
+import com.bgcoding.notes.app.ui.theme.Violet
+
+@Entity
+data class Note(
+    val title: String,
+    val content: String,
+    val timestamp: Long,
+    val color: Int,
+    @PrimaryKey val id: Int? = null
+) {
+    companion object {
+        val noteColors = listOf(RedOrange, LightGreen, Violet, BabyBlue, RedPink)
+    }
+}
