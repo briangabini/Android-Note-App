0604b5a80a1b265e84248cd8af97c2097713d5d1
Brian Gabini <91449132+briangabini@users.noreply.github.com>
2024-08-23 21:35:32 +0800

    feat: create a job to handle race conditions

--- app/src/main/java/com/bgcoding/notes/app/feature_note/presentation/notes/NotesViewModel.kt
+++ app/src/main/java/com/bgcoding/notes/app/feature_note/presentation/notes/NotesViewModel.kt
@@ -7,7 +7,9 @@ import androidx.lifecycle.viewModelScope
 import com.bgcoding.notes.app.feature_note.domain.model.Note
 import com.bgcoding.notes.app.feature_note.domain.use_case.NoteUseCases
 import com.bgcoding.notes.app.feature_note.domain.util.NoteOrder
+import com.bgcoding.notes.app.feature_note.domain.util.OrderType
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
@@ -22,6 +24,13 @@ class NotesViewModel @Inject constructor(
     val state: State<NotesState> = _state
 
     private var recentlyDeletedNote: Note? = null
+
+    // use a coroutine job so that we can cancel it when needed
+    private var getNotesJob: Job? = null
+
+    init {
+        getNotes(NoteOrder.Date(OrderType.Descending))
+    }
 
     fun onEvent(event: NotesEvent) {
         when(event) {
@@ -55,7 +64,8 @@ class NotesViewModel @Inject constructor(
     }
 
     private fun getNotes(noteOrder: NoteOrder) {
-        noteUseCases.getNotes(noteOrder)
+        getNotesJob?.cancel()
+        getNotesJob = noteUseCases.getNotes(noteOrder)
             .onEach { notes ->
                 _state.value = state.value.copy(
                     notes = notes,
