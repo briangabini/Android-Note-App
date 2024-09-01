bf3f190dda86ef1459e4f72dbf514c842ea4821c
Brian Gabini <91449132+briangabini@users.noreply.github.com>
2024-09-02 00:37:57 +0800

    feat: create an event class - AddEditNoteEvent

---
+++ app/src/main/java/com/bgcoding/notes/app/feature_note/presentation/add_edit_note/AddEditNoteEvent.kt
@@ -0,0 +1,13 @@
+package com.bgcoding.notes.app.feature_note.presentation.add_edit_note
+
+import androidx.compose.ui.focus.FocusState
+
+// for every single ui action, we create an event
+sealed class AddEditNoteEvent {
+    data class EnteredTitle(val value: String): AddEditNoteEvent()
+    data class ChangeTitleFocus(val focusState: FocusState): AddEditNoteEvent()
+    data class EnteredContent(val value: String): AddEditNoteEvent()
+    data class ChangeContentFocus(val focusState: FocusState): AddEditNoteEvent()
+    data class ChangeColor(val color: Int): AddEditNoteEvent()
+    data object SaveNote: AddEditNoteEvent()
+}
