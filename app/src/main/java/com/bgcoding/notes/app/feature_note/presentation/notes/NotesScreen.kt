f24dc1fe79ad5ba067a7e8f531342446116141a4
briangabini <briangabini@gmail.com>
2024-10-19 17:15:31 +0800

    fix: fix snackbar not closing

--- app/src/main/java/com/bgcoding/notes/app/feature_note/presentation/notes/NotesScreen.kt
+++ app/src/main/java/com/bgcoding/notes/app/feature_note/presentation/notes/NotesScreen.kt
@@ -29,6 +29,7 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarDuration
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.SnackbarResult
@@ -133,7 +134,8 @@ fun NotesScreen(
                                 val result = snackbarHostState
                                     .showSnackbar(
                                         message = "Note deleted",
-                                        actionLabel = "Undo"
+                                        actionLabel = "Undo",
+                                        duration = SnackbarDuration.Short
                                     )
                                 when (result) {
                                     SnackbarResult.ActionPerformed -> {
