f112ed3a72c44bf4bad3c5553f48b8b814dc0065
briangabini <briangabini@gmail.com>
2024-10-31 20:15:22 +0800

    feat: minor changes and implement Camera Preview

--- app/src/main/java/com/bgcoding/notes/app/feature_note/presentation/ocr/CameraScreen.kt
+++ app/src/main/java/com/bgcoding/notes/app/feature_note/presentation/ocr/CameraScreen.kt
@@ -1,35 +1,55 @@
 package com.bgcoding.notes.app.feature_note.presentation.ocr
 
+import android.content.Context
+import android.graphics.Bitmap
+import android.util.Log
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture.OnImageCapturedCallback
+import androidx.camera.core.ImageCaptureException
+import androidx.camera.core.ImageProxy
 import androidx.camera.view.CameraController
 import androidx.camera.view.LifecycleCameraController
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Cameraswitch
+import androidx.compose.material.icons.filled.Photo
+import androidx.compose.material.icons.filled.PhotoCamera
 import androidx.compose.material3.BottomSheetScaffold
-import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.SnackbarHost
-import androidx.compose.material3.SnackbarHostState
-import androidx.compose.material3.Text
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.rememberBottomSheetScaffoldState
-import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
+import androidx.core.content.ContextCompat
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.bgcoding.notes.app.feature_note.presentation.ocr.components.CameraPreview
+import com.bgcoding.notes.app.feature_note.presentation.ocr.components.PhotoBottomSheetContent
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CameraScreen(
-    navController: NavController
+    navController: NavController,
+    viewModel: CameraViewModel = hiltViewModel()
 ) {
+    val scope = rememberCoroutineScope()
     val scaffoldState = rememberBottomSheetScaffoldState()
-    val context = LocalContext.current                          // using this since we cannot access applicationContext inside a composable
+    val context = LocalContext.current
 
     val controller = remember {
         LifecycleCameraController(context).apply {
@@ -40,11 +60,17 @@ fun CameraScreen(
         }
     }
 
+    val bitmaps by viewModel.bitmaps.collectAsState()
+
     BottomSheetScaffold(
         scaffoldState = scaffoldState,
         sheetPeekHeight = 0.dp,
         sheetContent = {
-
+            PhotoBottomSheetContent(
+                bitmaps = bitmaps,
+                modifier = Modifier
+                    .fillMaxWidth()
+            )
         }
     ) { padding ->
         Box(
@@ -56,6 +82,78 @@ fun CameraScreen(
                 controller = controller,
                 modifier = Modifier.fillMaxSize()
             )
+
+            IconButton(
+                onClick = {
+                    controller.cameraSelector =
+                        if (controller.cameraSelector == CameraSelector.DEFAULT_BACK_CAMERA) {
+                            CameraSelector.DEFAULT_FRONT_CAMERA
+                        } else CameraSelector.DEFAULT_BACK_CAMERA
+                },
+                modifier = Modifier
+                    .offset(16.dp, 16.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Cameraswitch,
+                    contentDescription = "Switch camera"
+                )
+            }
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .align(Alignment.BottomCenter)
+                    .padding(16.dp),
+                horizontalArrangement = Arrangement.SpaceAround
+            ) {
+                IconButton(
+                    onClick = {
+                        scope.launch {
+                            scaffoldState.bottomSheetState.expand()
+                        }
+                    }
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Photo,
+                        contentDescription = "Open Gallery"
+                    )
+                }
+                IconButton(
+                    onClick = {
+                        takePhoto(
+                            context = context,
+                            controller = controller,
+                            onPhotoTaken = viewModel::onTakePhoto
+                        )
+                    }
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.PhotoCamera,
+                        contentDescription = "Take photo"
+                    )
+                }
+            }
         }
     }
+}
+
+fun takePhoto(
+    context: Context,
+    controller: LifecycleCameraController,
+    onPhotoTaken: (Bitmap) -> Unit
+) {
+    controller.takePicture(
+        ContextCompat.getMainExecutor(context),
+        object : OnImageCapturedCallback() {
+            override fun onCaptureSuccess(image: ImageProxy) {
+                super.onCaptureSuccess(image)
+                onPhotoTaken(image.toBitmap())
+            }
+
+            override fun onError(exception: ImageCaptureException) {
+                super.onError(exception)
+                Log.e("Camera", "Could not take photo: ${exception.message}")
+            }
+        }
+    )
 }
---
+++ app/src/main/java/com/bgcoding/notes/app/feature_note/presentation/ocr/CameraViewModel.kt
@@ -0,0 +1,25 @@
+package com.bgcoding.notes.app.feature_note.presentation.ocr
+
+import android.content.Context
+import android.graphics.Bitmap
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import dagger.hilt.android.qualifiers.ApplicationContext
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import javax.inject.Inject
+
+@HiltViewModel
+class CameraViewModel @Inject constructor(
+    @ApplicationContext private val context: Context
+) : ViewModel() {
+
+    private val _bitmaps = MutableStateFlow<List<Bitmap>>(emptyList())
+    val bitmaps = _bitmaps.asStateFlow()
+
+    fun onTakePhoto(bitmap: Bitmap) {
+        _bitmaps.value += bitmap
+    }
+
+    fun getContext(): Context = context
+}
